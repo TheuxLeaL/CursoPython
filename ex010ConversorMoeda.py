673d7aa53f9f91a0aad0062d7aa01ea863f4f61e
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 21:07:03 -0300

    first commit

---
+++ ex010ConversorMoeda.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+#Considere USS1.00 = 3.27
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 3.27
+print('Com R${:.2f} você pode comprar US$38.32')
---
+++ ex012CalculandoDesconto.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que leia o preso de um produto e mostre seu novo preço, com 5% de desconto.
+preço = float(input('Qual é o preço do produto? R$'))
+novo = preço - (preço * 5 / 100)
+print('O produto que custava R${}, na promoção com desconto de 5$ vai custar R${}'.format(preço, novo))
